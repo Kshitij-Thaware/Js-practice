e266a042c52b4c753873253b990e9533f9c36a82
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-02-29 11:25:07 +0000

    Opeations

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144
+let accountEmail = "smit@gmail.com"
+var accountPassword = "12345"  //Dont use for safety
+accountCity = "Jaipur" //Not recommended
+let accountstate
+//accountId = 345
+
+accountCity = "Nagpur"
+accountEmail = "hc@hc.com"
+accountPassword = "Kshitij"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //trat all JS code as newer version
+
+// alert("hello") //Cant work in nodejs
+
+let name = "Kshitij"
+let age = 20
+let isLoggedIn = false
+
+
+// Primitive Datatypes
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+//  symbol => unique
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
