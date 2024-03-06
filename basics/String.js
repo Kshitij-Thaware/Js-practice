148e8fca9d7e6451474aaf3f96fcea4dfa5620a7
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-06 12:39:22 +0000

    strings

--- basics/String.js
+++ basics/String.js
@@ -2,4 +2,4 @@ const name = "hitesh"
 const repoCount = 50
 //console.log(name + repoCount+ " Hello World");
 
-console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
