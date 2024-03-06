3694ca7b58c3865e87443155efd98ba31c20aeed
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-06 06:30:47 +0000

    Datatypes interview

---
+++ basics/datatypes-sum.js
@@ -0,0 +1,30 @@
+//  Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// 
+
+/*const isLoggedIn = false
+console.log(isLoggedIn);
+let user;
+console.log(user);
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+
+const bigNumber = 686469493496593469n
+
+*/
+// Reference Type (Non-Primitive)
+// Array, Objects, Functions
+
+const heroes = ["Shaktiman", "Nagraj", "doga"];
+let obj = {
+    name:"hitedh",
+    age : 22,
+}
+
+const func = function(){
+    console.log("Hello World");
+};
+console.log(typeof bigNumber)
