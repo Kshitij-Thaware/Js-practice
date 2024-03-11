fd525c4ada3258e8bfe73513ee8d7c12b8187e96
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-11 10:05:14 +0000

    dates

--- basics/dates.js
+++ basics/dates.js
@@ -6,3 +6,20 @@ let d = new Date()
 
 let myCreatedDate = new Date(2023, 0, 23,  21, 4, 45)
 console.log(myCreatedDate.toLocaleString());
+
+// Month's start from 0  in js
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default', {
+    weekday:"long",
+    Date: Date(),
+})
+
+console.log(newDate.weekday);
