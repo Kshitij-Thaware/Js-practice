db0111fd51051b8efd4edbbaf67f77442d1bba03
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-11 06:14:14 +0000

    num & math.

---
+++ basics/num_and_math.js
@@ -0,0 +1,30 @@
+// const score = 3200
+// console.log(score);
+
+// const balance = new Number(3200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+
+// const othernum = 123.567
+
+// console.log(othernum.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//+maths+//
+
+// console.log(Math.ceil(4.1))
+// console.log(Math.sqrt(4.1))
+// console.log(Math.floor(5.9))
+
+console.log(Math.random())
+console.log(Math.pow(4,5))
+
+const minn = 10
+const maxn = 20
+
+console.log(Math.floor(Math.random() * (maxn - minn + 1)) + minn);
