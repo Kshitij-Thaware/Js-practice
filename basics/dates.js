eae3797e79023f75b08c22e7f0590b5a6eec6c15
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-11 09:26:28 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,8 @@
+let d = new Date()
+// console.log(d.toString());
+// console.log(d.toDateString());
+// console.log(d.toLocaleString('IN-EN'));
+// console.log(typeof d);
+
+let myCreatedDate = new Date(2023, 0, 23,  21, 4, 45)
+console.log(myCreatedDate.toLocaleString());
