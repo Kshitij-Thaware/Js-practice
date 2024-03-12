53a68587a3c28f69b13d740de1246e6bfe8627c9
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-12 06:05:28 +0000

    array

--- basics_2/array.js
+++ basics_2/array.js
@@ -1,3 +1,36 @@
-const myarr = [0,1,2,4,5,8]
+//array
+const myarr = [0,1,2,3,4,5,6,7,8,9]
+const myEne = ['Nihal', 'Kshitij', 'Bhushan', 'Vishal']
+// console.log(myarr[4]);
+// console.log(myEne[2]);
 
-console.log(myarr[4]);
+const myArr2 = new Array(10,20,45,0,7,59,564,48,45,56)
+
+// console.log(myArr2);
+
+// myArr2.push(485)
+// myArr2.push(4)
+// myArr2.pop()
+
+// myarr.unshift(0)
+// myarr.shift()
+
+// console.log(myarr.includes(8));
+// console.log(myarr.indexOf(464));
+
+// const newArr = myarr.join()
+
+// console.log(myarr);
+// console.log(newArr)
+
+//slice, splice
+
+const myn1 = myarr.slice(1,4)
+console.log(myn1);
+console.log(myarr);
+
+const myn2 = myarr.splice(1,4)
+console.log(myn2);
+
+console.log(myarr);
+
