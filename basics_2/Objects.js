ff68491dd9f140f3eb7f749a30602bdc1f6bdb02
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-14 07:16:09 +0000

    Objects

---
+++ basics_2/Objects.js
@@ -0,0 +1,35 @@
+// Singleton
+
+const mySym = Symbol("key1")
+
+const juser ={
+    name: "Kshitij",
+    age:19,
+    [mySym]:"mykey1",
+    email: "kshitijthaw@gmail.com",
+    location: "Nagpur",
+    isLoggedIn: false
+}
+
+// console.log(juser);
+// console.log(juser.email);
+// console.log(juser["email"]);
+
+// console.log(typeof juser[mySym])
+
+// Object.freeze(juser)
+
+// juser.email ="nihal@wanjari.com"
+// console.log(juser["email"]);
+
+juser.greeting = function () {
+    console.log("Hello User welcome!!");
+}
+
+juser.greetingtwo = function () {
+    console.log(`Hello Js user, ${this.name}`);
+}
+
+console.log(juser.greeting())
+console.log(juser.greetingtwo())
+
