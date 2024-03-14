e4e4251ebd109f9ee9c87367b85b0528023b46f8
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-14 09:53:21 +0000

    functions

---
+++ basics_3/01_functions.js
@@ -0,0 +1,22 @@
+// function two(n1, n2){
+//     console.log(n1 + n2);
+// }
+
+function two(n1, n2){
+    // let result = (n1 + n2)
+    // return result
+    return n1 + n2
+}
+
+const result = two(5, 5)
+
+// console.log(result);
+
+function loginUserMessage(username = "user"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("kshitijthaware2004"))
