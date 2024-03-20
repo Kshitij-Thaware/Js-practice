17c38f088075888c4a7ebc3e03fdb6765c539e9e
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-20 06:33:15 +0000

    loops

--- iterations/one.js
+++ iterations/one.js
@@ -1,18 +1,60 @@
-// for (let index = 0; index <= 10; index++) {
-//     const element = index;
-//     console.log(element);
+// // for (let index = 0; index <= 10; index++) {
+// //     const element = index;
+// //     console.log(element);
     
+// // }
+
+
+
+// // Loops on array
+
+
+// // for of
+
+// const arr = [1, 2, 3, 5, 6, 7, 8]
+
+// for (const num of arr) {
+//     // console.log(num);
+// }
+
+// // Maps
+
+// const map = new Map()
+// map.set('In', "Indisa")
+// map.set('CH', "China")
+// map.set('NZ', "New Zealand")
+// map.set('In', "Indisa")
+// // console.log(map)
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
 // }
 
 
+// const myObject = {
+//     'game1' : 'NFS',
+//     'game2' : 'GTA'
+// }
 
-// Loops on array
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
 
 
-// for of
+// ForIN
+const muObject = {
+    js: 'Javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    html: 'HTML by vs'
+ }
 
-const arr = [1, 2, 3, 5, 6, 7, 8]
+// for (const key in muObject) {
+//     console.log(`${key} filetype is for ${muObject[key]}`);
+// }
 
-for (const num of arr) {
-    console.log(num);
+const prog = ["js", "rb", "py", "java"]
+
+for (const key in prog) {
+    console.log(prog[key]);
 }
