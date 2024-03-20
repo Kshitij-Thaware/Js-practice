989538dfad5664651777fbeaa612a32421f40bd2
Kshitij-Thaware <161702565+Kshitij-Thaware@users.noreply.github.com>
2024-03-20 10:33:50 +0000

    lo

--- iterations/two.js
+++ iterations/two.js
@@ -14,12 +14,31 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //     return num > 4
 // })
 
-const newnums =  []
+// const newnums =  []
 
-myNums.forEach((num) => {
-    if(num > 4){
-        newnums.push(num)
-    }
-})
+// myNums.forEach((num) => {
+//     if(num > 4){
+//         newnums.push(num)
+//     }
+// })
 
-console.log(newnums);
+// console.log(newnums);
+
+const books = [
+    {title: 'Book One', genre: 'Fiction', publish:1981,
+    edition: 2004},
+    {title: 'Book Two', genre: 'Non-Fiction', publish:1985,
+    edition: 2005},
+    {title: 'Book Three', genre: 'Horror', publish:1986,
+    edition: 2006},
+    {title: 'Book Four', genre: 'Comedy', publish:1981,
+    edition: 2007},
+    {title: 'Book Five', genre: 'Non-Fiction', publish:1985,
+    edition: 2008},
+    {title: 'Book Six', genre: 'Science', publish:1986,
+    edition: 2009},
+    {title: 'Book Seven', genre: 'Fiction', publish:1981,
+    edition: 2001},
+    {title: 'Book Eight', genre: 'History', publish:1985,
+    edition: 2002},
+]
